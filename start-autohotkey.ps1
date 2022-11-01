b13cba99e8b64b0519f2570b6e356705ef653833
Christian Thillemann <christian.thillemann@unit4.com>
2022-11-01 17:24:12 +0100

    start autohotkey in background

--- start-autohotkey.ps1
+++ start-autohotkey.ps1
@@ -1,3 +1,4 @@
 # start autohotkey - requires
 # e.g. scoop install autohotkey ()
-autohotkey .\AutoHotkey\AutoHotkey.ahk
+# old: autohotkey .\AutoHotkey\AutoHotkey.ahk
+Start-Process autohotkey "C:\kedde\win-dot-files\AutoHotkey\autohotkey.ahk" -WorkingDirectory "c:\kedde\win-dot-files\" -WindowStyle Hidden
